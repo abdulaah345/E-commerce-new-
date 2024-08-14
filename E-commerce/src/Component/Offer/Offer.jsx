300c073a78d6a2c9af624605f14dc2a85e0100ac
abdullah345 <abdallahmohamed54655@gmail.com>
2024-08-14 16:32:06 +0300

    v2

--- E-commerce/src/Component/Offer/Offer.jsx
+++ E-commerce/src/Component/Offer/Offer.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import logo5 from'./../../assets/image-removebg-preview (8) 1.png'
+import logo6 from'./../../assets/Group 58.png'
+import './../Offer/Offer.css'
 const Offer = () => {
   return (
-    <div>
-      <h1>offfffffffffffffffffffffffrt</h1>
-    </div>
+    <>
+<div className="offer-banner">
+            <div className="logo">
+                <img  className="pho"src={logo6} alt="SellKick Logo" />
+            </div>
+            <div className="offer-details">
+                <h1>Offers</h1>
+                <h2>OFFERS TODAY</h2>
+                <p>Buy now and get up to 50% off</p>
+            </div>
+            <div className="offer-icon">
+                <img src={logo5} alt="Offer Icon" />
+            </div>
+        </div>
+    </>
   )
 }
 
